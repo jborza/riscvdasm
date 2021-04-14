3a032951295eb3ecc96c0fe38b35fb85364568fb
jborza <jborza@gmail.com>
2021-04-14 19:59:21 +0200

    if argv[1] is provided, disassembles a file by its name

--- main.c
+++ main.c
@@ -2,26 +2,62 @@
 #include <unistd.h>
 #include "disassembler.h"
 
-int main(int argc, char* argv[]){
+void process_word(uint32_t offset, FILE *fd)
+{
+    printf("%8x:", offset);
+    uint32_t op = 0u;
+
+    op |= getc(stdin);
+    op |= getc(stdin) << 8;
+    op |= getc(stdin) << 16;
+    op |= getc(stdin) << 24;
+
+    printf("\t%08x\t\t", op);
+    disassemble_op(&op, offset);
+}
+
+void process_stdin()
+{
     //read input binary from stdin, output text to stdout
     uint32_t offset = 0;
-    while(1){
-        printf("%8x:", offset);
-        uint32_t op = 0u;
-
-        op |= getc(stdin);            
-        op |= getc(stdin) << 8;       
-        op |= getc(stdin) << 16;     
-        op |= getc(stdin) << 24;      
-        
-        printf("\t%08x\t\t", op);
-        disassemble_op(&op, offset);
+    while (1)
+    {
+        process_word(offset, stdin);
         offset += 4;
 
         int dummy = getc(stdin);
-        if(dummy == EOF)
+        if (dummy == EOF)
             break;
         ungetc(dummy, stdin);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        //read from standard input, output to standard output
+        process_stdin();
+    }
+    else
+    {
+        //use input file name
+        FILE *fd = fopen(argv[1], "rb");
+        if (!fd)
+        {
+            printf("Error opening input file.\n");
+            return 1;
+        }
+        fseek(fd, 0, SEEK_END);
+        size_t size = ftell(fd);
+        rewind(fd);
+        uint32_t offset = 0;
+        while (offset < size)
+        {
+            process_word(offset, fd);
+            offset += 4;
+        }
+        fclose(fd);
     }
     return 0;
 }
